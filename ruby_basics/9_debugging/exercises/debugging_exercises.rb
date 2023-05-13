4ebfc8481ad311a0faf1c8999f5798581f17eb3b
Joe Fogiato <joe.fogiato@gmail.com>
2023-05-13 14:44:13 -0400

    Add: all solutions to the devugging exercises

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -6,11 +6,11 @@
 # To the top, the first line, where the error occured, and ONLY THEN fix the error
 
 def decrement_smallest_value(nested_array)
-  smallest_value = nested_array.flatten.max
+  smallest_value = nested_array.flatten.max # gets 4
   nested_array.each do |array|
     array.each do |current_value|
       if smallest_value > current_value
-        smallest_value = current_valu
+        smallest_value = current_value
       end
     end
   end
@@ -24,7 +24,7 @@ def increment_greatest_value(nested_array)
   greatest_value = nested_array.flatten.min
   nested_array.each do |array|
     array.each do |current_value|
-      if greatest_value < nil
+      if greatest_value < current_value
         greatest_value = current_value
       end
     end
@@ -36,8 +36,8 @@ end
 # Use p and puts in order to find what's wrong with our method
 
 def isogram?(string)
-  original_length = string.length
-  string_array = string.downcase.split
+  original_length = string.length # 4
+  string_array = string.downcase.split('')
   unique_length = string_array.uniq.length
   original_length == unique_length
 end
@@ -52,5 +52,5 @@ end
 def yell_greeting(string)
   name = string
   name = name.downcase
-  greeting = "WASSAP, #{name}!"
+  greeting = "WASSAP, #{name.upcase}!"
 end
